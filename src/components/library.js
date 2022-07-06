5c221245efefad29185b7f45078bd1542d6b6f61
Ravi <samcollins9899@gmail.com>
2022-07-06 12:01:00 +0530

    added gallery styles

--- src/components/library.js
+++ src/components/library.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import LibrarySongs from "./librarySongs";
 
-const Library =({songs})=>{
+const Library =({songs, setCurrentSong })=>{
 
     return(
         <div className="library">
             <h2>Library</h2>
-            <div className="library-songs">
+            <div >
                 {songs.map( song =>
-                    <LibrarySongs song={song } />
+                    <LibrarySongs  id={song.id} key={song.id} songs={songs} setCurrentSong ={setCurrentSong}song={song } />
                     )}
 
             </div>
